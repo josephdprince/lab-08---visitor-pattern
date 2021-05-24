5536053a6d8c57567b466337021e9ac2f627f1dc
puloma-k <puloma@pulomakatiyar.com>
2021-05-23 18:51:45 -0700

    Fixed return type

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -4,6 +4,8 @@
 #include "visitor.hpp"
 #include "iterator.hpp"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 #include "op.hpp"
 #include "Rand.hpp"
@@ -15,85 +17,91 @@
 
 
 class VisitorLaTeX : public Visitor {
+    private:
+	std::string equation;
     public:
 	virtual void visit_op(Op* node) {
-		std::cout << "{" << node->evaluate() << "}";
+		std::ostringstream out;
+		out << std::setprecision(6) << node->evaluate();
+		equation += ("{" + out.str() + "}");
 	}
 
         virtual void visit_rand(Rand* node) {
-		std::cout << "{" << node->evaluate() << "}";
+		std::ostringstream out;
+		out << std::setprecision(6) << node->evaluate();
+		equation += ("{" + out.str() + "}");
 	}
 	
 	virtual void visit_add_begin(Add* node) {
-		std::cout << "{(";
+		equation +=  "{(";
 	}
 
         virtual void visit_add_middle(Add* node) {
-		std::cout << "+";		
+		equation += "+";		
 	}
 
         virtual void visit_add_end(Add* node) {
-		std::cout << ")}";
+		equation += ")}";
 	}
 
         virtual void visit_sub_begin(Sub* node) {
-		std::cout << "{(";
+		equation += "{(";
 	}
 
         virtual void visit_sub_middle(Sub* node) {
-		std::cout << "-";
+		equation += "-";
 	}
 
         virtual void visit_sub_end(Sub* node) {
-		std::cout << ")}";
+		equation += ")}";
 	}
 
         virtual void visit_mult_begin(Mult* node) {
-		std::cout << "{(";
+		equation += "{(";
 	}
 
         virtual void visit_mult_middle(Mult* node) {
-		std::cout << "\\cdot";
+		equation += "\\cdot";
 	}
 
         virtual void visit_mult_end(Mult* node) {
-		std::cout << ")}";
+		equation += ")}";
 	}
 
         virtual void visit_div_begin(Div* node) {
-		std::cout << "{\\frac";
+		equation +=  "{\\frac";
 	}
 
         virtual void visit_div_middle(Div* node){
-		std::cout << "";
+		equation +=  "";
 	}
 
         virtual void visit_div_end(Div* node) {
-		std::cout << "}";
+		equation +=  "}";
 	}
 
         virtual void visit_pow_begin(Pow* node) {
-		std::cout << "{(";
+		equation += "{(";
 	}
 
         virtual void visit_pow_middle(Pow* node) {
-		std::cout << "^";
+		equation +=  "^";
 	}
 
         virtual void visit_pow_end(Pow* node) {
-		std::cout << ")}";
+		equation += ")}";
 	}
 
 	std::string PrintLaTeX(Base* ptr) {
+		equation = "$";
 		Iterator* it = new Iterator(ptr);
-		std::cout << "$";
         	while (it->is_done() == false) {
                 	Base* curr = it->current_node();
                 	curr->accept(this, it->current_index());
                 	it->next();
         	}
-		std::cout << "$" << std::endl;
-
+		equation += "$";
+		return equation;
 	}
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #include "VisitorLaTeX.hpp"
 
 int main() {
-	/*
+	
 	//Ex 1:
 	Op* op1 = new Op(1);
 	Op* op2 = new Op(5);
@@ -23,57 +23,56 @@ int main() {
 	cout << add->stringify() << " => ";
 
 	VisitorLaTeX* vist_1 = new VisitorLaTeX();
-	vist_1->PrintLaTeX(add);
-
+	cout << vist_1->PrintLaTeX(add) << endl;
+	
 	
 	//Ex 2:
-	Op* op1 = new Op(5);
-	Op* op2 = new Op(2);
+	Op* op4 = new Op(5);
+	Op* op5 = new Op(2);
 
-	Pow* pow = new Pow(op1, op2);
+	Pow* pow = new Pow(op4, op5);
 
 	cout << pow->stringify() << " => ";
 	
 	VisitorLaTeX* vist_2 = new VisitorLaTeX();
-	vist_2->PrintLaTeX(pow);
+	cout << vist_2->PrintLaTeX(pow) << endl;
 	
-
+	
 	//Ex 3:
-	Op* op1 = new Op(2);
-	Op* op2 = new Op(5);
+	Op* op6 = new Op(2);
+	Op* op7 = new Op(5);
 
-	Mult* mult = new Mult(op1, op2);
+	Mult* mult = new Mult(op6, op7);
 
 	cout << mult->stringify() << " => ";
 
 	VisitorLaTeX* vist_3 = new VisitorLaTeX();
-	vist_3->PrintLaTeX(mult);
-
+	cout << vist_3->PrintLaTeX(mult) << endl;
 	
 	//Ex 4:
-	Op* op1 = new Op(2);
-        Op* op2 = new Op(3);
+	Op* op8 = new Op(2);
+        Op* op9 = new Op(3);
 
-        Div* div = new Div(op1, op2);
+        Div* div = new Div(op8, op9);
 
         cout << div->stringify() << " => ";
 
         VisitorLaTeX* vist_4 = new VisitorLaTeX();
-        vist_4->PrintLaTeX(div);
+        cout << vist_4->PrintLaTeX(div) << endl;
 
-	*/
-	Op* op1 = new Op(2);
-        Op* op2 = new Op(5);
+	//Ex 5:
+	Op* op10 = new Op(2);
+        Op* op11 = new Op(5);
 
-        Mult* mult = new Mult(op1, op2);
-	Pow* pow = new Pow(op2, op1);
+        Mult* mult2 = new Mult(op10, op11);
+	Pow* pow2 = new Pow(op11, op10);
 
-	Div* div = new Div(mult, pow);
+	Div* div2 = new Div(mult2, pow2);
 
-        cout << div->stringify() << " => ";
+        cout << div2->stringify() << " => ";
 
         VisitorLaTeX* vist_5 = new VisitorLaTeX();
-        vist_5->PrintLaTeX(div);
-
+        cout << vist_5->PrintLaTeX(div2) << endl;
+	
 	return 0;
 }
